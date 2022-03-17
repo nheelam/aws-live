4ef8dde23067ed8779df2bc0d3dbd6b7d841774d
nheelam <64530832+nheelam@users.noreply.github.com>
2022-03-17 14:40:37 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.cb2xuycq0ncu.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "nheelammitthera-employee"
+customregion = "us-east-1"
